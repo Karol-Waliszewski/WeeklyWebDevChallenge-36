e27ae997a1fd936e8065441e4ec9f6517cd5ebaf
Karol <karolwaliszewski@gmail.com>
2017-12-29 12:41:13 +0100

    Responsive menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,10 @@
-const expandElement = function (ev) {
-  console.log(ev)
+const expandElement = function(ev) {
+
   ev.target.parentNode.classList.toggle('expand');
 }
 
-document.querySelector('#categories>.sidebar__heading').addEventListener('click',expandElement);
-document.querySelector('#filter>.sidebar__heading').addEventListener('click',expandElement);
+document.querySelector('#categories>.sidebar__heading').addEventListener('click', expandElement);
+document.querySelector('#filter>.sidebar__heading').addEventListener('click', expandElement);
+document.querySelector('.navigation__hamburger').addEventListener('click', function(ev) {
+  ev.target.parentNode.parentNode.classList.toggle('expand');
+});
